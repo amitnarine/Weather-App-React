f76966c28e7b609d46885832eed87c4d43bb55ee
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-09-26 16:53:38 -0400

    Setting up API calls to openweather

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+  const[data,setData] = useState({});
+  const[location,setLocation] = useState('');
 
-//const url ='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=986caa9876ece6b73fc38c20cb0e529c'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=986caa9876ece6b73fc38c20cb0e529c`;
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+  axios.get(url).then((response) => {
+    
+    setData(response.data)
+    console.log(response.data)
+  })
+  setLocation('');
+}
+
+}
 
   return (
     <div className="app">
+
+      <div className="search">
+
+      <input 
+      
+      value={location}
+      onChange={event => setLocation(event.target.value)}
+      placeholder = 'Enter a location'
+      onKeyPress={searchLocation}
+
+      
+      type="text" />
+
+
+        
+      </div>
       <div className="container">
         <div className="top">
 
